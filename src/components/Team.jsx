467c5ed8133dd731b82bbf3fc96764a053087ddc
Anup Adhikari <anup.211344@ncit.edu.np>
2024-12-21 08:55:20 +0545

    added chairperson

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,29 +7,30 @@ import {
   Typography,
   Tooltip,
 } from "@material-tailwind/react";
+import Deepak from '/deepak.jpg';
 
 export default function Team() {
   const team = [
     {
-      name: 'Aarav Sharma',
+      name: 'Deepak Joshi Pokhrel',
       role: 'Managing Director',
-      image: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80'
+      
     },
     {
-      name: 'Priya Patel',
-      role: 'Programs Director',
+      name: 'Bir Bahadur Ale',
+      role: 'Founder/Chairperson  ',
     },
     {
-      name: 'Rajesh Thapa',
-      role: 'Advocacy Manager',
+      name: 'Mohan Adhikari',
+      role: 'Member',
     },
     {
-      name: 'Maya Gurung',
-      role: 'Community Outreach',
+      name: 'Madhu Thapa Magar Rana',
+      role: 'Member',
     },
     {
-      name: 'Bikash KC',
-      role: 'Research Coordinator',
+      name: 'Devu Thapa Magar',
+      role: 'Member',
     }
   ];
 
@@ -52,7 +53,7 @@ export default function Team() {
           {/* Left Section: Featured Member */}
           <Card className="w-full lg:w-4/5 mx-auto">
             <CardHeader floated={false} className="h-80">
-              <img src={featuredMember.image} alt={featuredMember.name} className="w-full h-full object-cover" />
+              <img src={Deepak} alt={featuredMember.name} className="w-full h-full object-cover" />
             </CardHeader>
             <CardBody className="text-center">
               <Typography variant="h4" color="blue-gray" className="mb-2">
@@ -66,7 +67,7 @@ export default function Team() {
 
           {/* Right Section: Other Members */}
           <div className="bg-gray-100 rounded-lg p-6 shadow-lg">
-            <h3 className="text-2xl font-semibold text-gray-800 mb-4">Other Team Members</h3>
+            <h3 className="text-2xl font-semibold text-gray-800 mb-4">Team Members</h3>
             <ul className="space-y-4">
               {otherMembers.map((member) => (
                 <li
